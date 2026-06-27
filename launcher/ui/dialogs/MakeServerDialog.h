0832cac8da65784eb5a5b51436ca07e0be6facb5
pale_king <mrario1391@gmail.com>
2026-06-27 04:11:57 +0330

    Create MakeServerDialog.h

---
+++ launcher/ui/dialogs/MakeServerDialog.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <QDialog>
+
+class MakeServerDialog : public QDialog {
+    Q_OBJECT
+
+public:
+    explicit MakeServerDialog(QWidget *parent = nullptr);
+    ~MakeServerDialog();
+
+private slots:
+    void on_customButtonClicked();
+    void on_okButtonClicked();
+    void on_cancelButtonClicked();
+
+private:
+    void setupUI();
+    
+    // UI elements
+    class Ui_MakeServerDialog *ui;
+};
